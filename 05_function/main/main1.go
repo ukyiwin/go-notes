2d53edd5dfbba9149e70e30f399a60faa2aba525
Golangmy <golang.my@gmail.com>
2018-01-15 21:41:57 +0630

    /*varadic param*/

--- 05_function/main/main1.go
+++ 05_function/main/main1.go
@@ -5,6 +5,11 @@ import "fmt"
 func main() {
 	var a = average(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 	fmt.Println(a)
+
+	/*varadic param*/
+	data := []float64{12, 24, 2, 23, 12, 1, 334, 23, 12, 12, 3, 34}
+	b := average(data...)
+	fmt.Println(b)
 }
 
 func average(x ...float64) float64 {
